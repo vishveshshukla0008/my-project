3079c1a2b29a1043485096b3066c6ccc83ba1e6a
Yash <vishveshshukla82@gmail.com>
2024-07-29 12:46:51 +0530

    deleting commits

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // add new feature : (bvuttin)
 // add new feature : (form)
+function sub() {
+    let a = 10;
+    let b = 20;
+    return a - b;
+}
+sub();
+// add new feature : (form)
+
+function sum() {
+    let a = 10;
+    let b = 20;
+    return a + b;
+}
+sum();
+
